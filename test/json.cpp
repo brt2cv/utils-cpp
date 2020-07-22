15fdfee6042b76f64aae66f2a8a0ef549ab18ae7
Bright@E49 <brt2@qq.com>
2020-07-23 07:38:22 +0800

    调整了json模块的调用

--- test/json.cpp
+++ test/json.cpp
@@ -1,12 +1,11 @@
 #pragma execution_character_set("utf-8")
 
 #include <fstream>
-#include <iomanip>
+#include <iomanip>  // std::setw
 
 #include "utils.h"
 #include "json.hpp"
-using json = nlohmann::json;
-using namespace std;
+using Json = nlohmann::json;
 
 string path_demofile = "test/json_demo.json";
 
@@ -30,7 +29,7 @@ int main(){
 
     Assert(os::exists(path_demofile), strfmt("不存在文件【{}】", path_demofile))
     std::ifstream i(path_demofile);
-    json j;
+    Json j;
     i >> j;
     print(">> Load the Json file: ", j.dump());
 
